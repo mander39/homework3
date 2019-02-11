b700b4913c64eb3d3d9bc3ed9fe4ca3dc587fb64
tomthetank46 <tomhogrefe46@gmail.com>
2019-02-11 00:49:18 -0500

    parks to php

---
+++ homework3/parks.php
@@ -0,0 +1,119 @@
+<?php include 'inc/html-top.inc'; ?>
+<?php include 'inc/nav.inc'; ?>
+
+	<img src="images/parksimage.jpg" alt="parks" class="center">
+	<h1>National Parks</h1>
+
+	<p>
+		A national park is a park in use for conservation purposes. Often it is a reserve of natural, semi-natural, or developed land that a sovereign state declares or owns. Although individual nations designate their own national parks differently, there is a common idea: the conservation of 'wild nature' for posterity and as a symbol of national pride. An international organization, the International Union for Conservation of Nature (IUCN), and its World Commission on Protected Areas (WCPA), has defined "National Park" as its Category II type of protected areas.
+
+		The United States has many National Parks -- areas set aside for preservation, often for their natural beauty. These areas make popular tourist attractions, both for their historical value and for their hiking and camping attractions. I've been lucky enough to visit a few national parks, some of them are listed below.
+	</p>
+
+	<h2>Grand Canyon</h2>
+
+	<h3>History</h3>
+
+	<p>
+		Grand Canyon National Park, located in northwestern Arizona, is the 15th site in the United States to have been named a national park. The park's central feature is the Grand Canyon, a gorge of the Colorado River, which is often considered one of the Wonders of the World. The park, which covers 1,217,262 acres (1,901.972 sq mi; 4,926.08 km2) of unincorporated area in Coconino and Mohave counties, received more than six million recreational visitors in 2017, which is the second highest count of all American national parks after Great Smoky Mountains National Park. The Grand Canyon was designated a World Heritage Site by UNESCO in 1979.
+	</p>
+
+	<h3>Geography</h3>
+
+	<p>
+		The Grand Canyon, including its extensive system of tributary canyons, is valued for its combination of size, depth, and exposed layers of colorful rocks dating back to Precambrian times. The canyon itself was created by the incision of the Colorado River and its tributaries after the Colorado Plateau was uplifted, causing the Colorado River system to develop along its present path.
+	</p>
+
+	<p>
+		The primary public areas of the park are the South and North Rims, and adjacent areas of the canyon itself. The rest of the park is extremely rugged and remote, although many places are accessible by pack trail and backcountry roads. The South Rim is more accessible than the North Rim, and accounts for 90% of park visitation.
+	</p>
+
+	<p>
+		The park headquarters are at Grand Canyon Village, not far from the south entrance to the park, near one of the most popular viewpoints.
+	</p>
+
+	<h3>Trails</h3>
+
+	<ul>
+
+		<li>Bright Angel Point Trail</li>
+		<li>Cape Final Trail</li>
+		<li>Cape Royal Trail</li>
+		<li>Cliff Spring Trail</li>
+		<li>Fire Point Trail</li>
+		<li>Francois Matthes Trail</li>
+		<li>Ken Patrick Trail</li>
+		<li>Komo Point Trail</li>
+		<li>Rim Trail</li>
+		<li>Tiyo Point Trail</li>
+		<li>Transept Trail</li>
+		<li>Uncle Jim Trail</li>
+		<li>Walhalla Glades Trail</li>
+		<li>Walhalla Spur Trail</li>
+		<li>Widforss Trail</li>
+	</ul>
+
+	<h2>Yosemite</h2>
+
+	<h3>History</h3>
+
+	<p>
+		Yosemite National Park** is an American national park located in the western Sierra Nevada of Central California bounded on the southeast by Sierra National Forest and on the northwest by Stanislaus National Forest. The park, which is managed by the National Park Service, covers an area of 747,956 acres (1,168.681 sq mi; 302,687 ha; 3,026.87 km2) and sits in four counties: centered in Tuolumne and Mariposa, extending north and east to Mono and south to Madera County. Designated a World Heritage site in 1984, Yosemite is internationally recognized for its granite cliffs, waterfalls, clear streams, giant sequoia groves, lakes, mountains, meadows, glaciers, and biological diversity. Almost 95% of the park is designated wilderness.
+	</p>
+
+	<h3>Geography</h3>
+
+	<p>
+		Almost all of the landforms in the Yosemite area are cut from the granitic rock of the Sierra Nevada Batholith (a batholith is a large mass of intrusive igneous rock that formed deep below the surface). About 5% of the park's landforms (mostly in its eastern margin near Mount Dana) are metamorphosed volcanic and sedimentary rocks. These rocks are called *roof pendants* because they were once the roof of the underlying granitic rock.
+	</p>
+
+	<h3>Trails</h3>
+
+	<ul>
+		<li>Bridalveil Fall Trail</li>
+		<li>Lower Yosemite Fall Trail</li>
+		<li>Cook's Meadow Loop</li>
+		<li>Mirror Lake Loop</li>
+		<li>Valley Loop Trail</li>
+		<li>Vernal Fall and Nevada Fall Trails</li>
+		<li>Yosemite Falls Trail</li>
+		<li>Snow Creek Trail</li>
+		<li>Four Mile Trail</li>
+		<li>Half Dome Trail</li>
+	</ul>
+
+	<h2>Acadia</h2>
+
+	<h3>History</h3>
+
+	<p>
+		Acadia National Park is an American national park located in the state of Maine, southwest of Bar Harbor. The park preserves about half of Mount Desert Island], many adjacent smaller islands, and part of the Schoodic Peninsula on the coast of Maine. Acadia was initially designated Sieur de Monts National Monument by proclamation of President Woodrow Wilson] in 1916. Sieur de Monts was renamed and redesignated Lafayette National Park by Congress in 1919—the first national park in the United States east of the Mississippi River and the only one in the Northeastern United States. The park was renamed Acadia National Park in 1929. More than 3.5 million people visited the park in 2017.
+	</p>
+
+	<h3>Geography</h3>
+
+	<p>
+		The park includes mountains, an ocean coastline, woodlands, lakes, and ponds. In addition to nearly half of Mount Desert Island, the park designation also preserves a portion of the Schoodic Peninsula on the mainland, as well as the majority of Isle au Haut and Baker Island, all of Bar Island, three of the four Porcupine Islands (Sheep, Bald and Long), the Thrumcap (an islet), part of Bear Island, and Thompson Island in Mount Desert Narrows, as well as several other islands and islets. Bar Island, which can be visited on foot over a sandbar around low tide, and the Porcupine Islands are in Frenchman Bay by Bar Harbor. About 57 mi (92 km) of carriage roads were designed and financed by John D. Rockefeller Jr. on Mount Desert Island, 45 miles (72 km) of which continue to be maintained inside the park.
+	</p>
+
+	<h3>Trails</h3>
+	<ul>
+		<li>Bar Harbor Shore Path</li>
+		<li>Cadillac Summit</li>
+		<li>Jordan Pond Nature Trail</li>
+		<li>Ocean Trail</li>
+		<li>Wonderland</li>
+		<li>Bar Island</li>
+		<li>Most Carriage Roads</li>
+		<li>Ship Harbor Nature Trail</li>
+	</ul>
+</div>
+<footer class="slant">
+	Thomas Hogrefe - In-class Lab 3: Markdown and Information Architecture<br>Content from Wikipedia  - retrieved 02/09/2019
+</footer>
+
+<?php include 'inc/html-bottom.inc'; ?>
+
+
+
+

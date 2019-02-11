6bbb2bd3ddb71ee05f0f14f37e7cd7f8c2212a86
lukeoluoch <47185625+lukeoluoch@users.noreply.github.com>
2019-02-10 19:29:58 -0500

    Create index.php

---
+++ homework3/index.php
@@ -0,0 +1,22 @@
+<?php include 'inc/html-top.inc'; ?>
+<?php include 'inc/nav.inc'; ?>
+<h1 class="homepage">CSC 174 Assignment 3</h1>
+
+<a href ="sports.php"> 
+    <h2 class="homepage">American Sports Leagues</h2>
+    <img src="images/banner2.png" alt="lebron james" class="center"> 
+    <p>Major League Baseball, the National Football League, and the National Basketball Association are the three largest professional sports leagues in the United States. They are the three largest sports leagues in the world by revenue, and field players from all around the world. Every block of competition is broken into an annual season where one champion is crowned out of all competitors. For the three leagues, seasons vary greatly in terms of games played. The NFL, for instance, only plays 16 games per season per team, while the NBA plays 82, and the MLB plays 162 games per season per team. At the end of each season, the leagues culminate in a playoff where the best teams compete for that season's championshop title.</p>
+
+</a>
+
+<a href ="platformers.php">
+    <h2 class="homepage">Video Game Platformers</h2>
+    <img src="images/platformbanner.png" alt="logos" class="center">
+    <p> Platformers are a genre of videogame that came to be in the early 80s.They are characterized by having stylized and iconic main characters and gameplay mechanics that involve jumping and avoiding obstacles. The have existed in both 2D and 3D forms and have spanned all generations of video game hardware since their inception. </p>
+
+</a>
+
+
+<footer class= "slant">Team Timbuktu - In-class Lab 3: Markdown and Information Architecture</footer>
+
+<?php include 'inc/html-bottom.inc'; ?>
